1dc0cf737d62a3045ecc076f734fad8242ffb7a8
maxhol23 <100097037+maxhol23@users.noreply.github.com>
2022-02-22 12:20:17 +1300

    First time using Python and Power shell and learning how to use the both of them using simple coding
    First time using Python and Power shell and learning how to use the both of them using simple coding

---
+++ 1_print_exercises.py
@@ -0,0 +1,32 @@
+#Prints out Max Holgate
+print("Max Holgate")
+
+#Prints out(Luke Smith)
+print("Luke Smith")
+
+#Prints out(Kings High School)
+print("Kings High School")
+
+#Prints out(15)
+print("15")
+
+#Prints out(30 hours)
+print("30 hours")
+
+#Calculates and Prints out(2047)
+print(2022+25)
+
+#Calculates and Prints out(115)
+print(15+100)
+
+#Calculates and Prints out(24)
+print(658/7)
+
+#Calculates and Prints out(53)
+print(2022-1969)
+
+#Prints out my favourtie video game (FIFA)
+print("FIFA")
+
+#Prints out my favourtie subject (Accountintg)
+print("Accounting")
