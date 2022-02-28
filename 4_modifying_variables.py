b202f18c84d3321a50f7e4721d90e66df2bff0a4
maxhol23 <100097037+maxhol23@users.noreply.github.com>
2022-03-01 10:34:04 +1300

    modifying variable
    Today I learnt how to modify variable and I mainly focused on using it regarding money

---
+++ 4_modifying_variables.py
@@ -0,0 +1,52 @@
+money=200
+hat=20
+top=30
+pants=15
+belt=60
+shoes=40
+#2
+print(money)
+#3
+print("Buying A Hat")
+#4
+money=money-hat
+#5
+print("You now have the following amount left over")
+#6
+print(money)
+
+#7
+print("Buying a top")
+
+money=money-top
+
+print("You now have the following amount left over")
+
+print(money)
+
+#8
+print("Buying pants")
+
+money=money-pants
+
+print("You now have the following amount left over")
+
+print(money)
+
+#9
+print("Buying belts")
+
+money=money-belt
+
+print("You now have the following amount left over")
+
+print(money)
+
+#10
+print("Buying shoes")
+
+money=money-shoes
+
+print("You now have the following amount left over")
+
+print(money)
